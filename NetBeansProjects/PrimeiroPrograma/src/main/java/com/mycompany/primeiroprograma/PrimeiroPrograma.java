f3eaa68b2fc4ec22cfc0ba806dae9f1d71aa4bd2
TIAGO BURLAMAQUE SOUSA FILHO <tiago_sousa-filho@estudante.sesisenai.org.br>
2025-04-22 13:26:28 -0300

    Aula de java

---
+++ NetBeansProjects/PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package com.mycompany.primeiroprograma;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("olá mundo");
+    }
+}
+/*printIn: para mostrar na tela e pular uma linha para baixo*/
+/*print: mostra na tela*/
+/*psvm + tab: para fazer o ,public static void main*/
+/*sout + tab: para fazer o, System.out.println("");*/
